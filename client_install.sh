44c897ca68d64e56b186bc5eaed96480e06100a0
Bikram Adhikari <salik.adhikari@gmail.com>
2014-03-21 13:58:30 +0545

    modified to place logstash on opt folder

--- client_install.sh
+++ client_install.sh
@@ -1,8 +1,7 @@
 sudo apt-get update
 sudo apt-get -y install openjdk-7-jdk
-wget 54.229.193.9/logstash.jar
-mkdir logstash
-cd logstash
-wget 54.229.193.9/logstash.jar
-wget https://raw.githubusercontent.com/meadhikari/logstash/master/sample_shipper.conf
+sudo mkdir /var/opt/logstash
+cd /var/opt/logstash
+sudo wget 54.229.193.9/logstash.jar
+sudo wget https://raw.githubusercontent.com/meadhikari/logstash/master/sample_shipper.conf
 #java -jar logstash.jar agent --verbose -f sample_shipperconf
