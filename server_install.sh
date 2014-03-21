6bc6f02668e0e7abe3e8272812af353286706c4b
Bikram Adhikari <salik.adhikari@gmail.com>
2014-03-21 23:16:47 +0545

    added init part to the server_install script

--- server_install.sh
+++ server_install.sh
@@ -21,6 +21,14 @@ sudo dpkg -i elasticsearch-1.0.0.deb
 #downloading of configuration file for the indexer
 sudo wget https://raw.githubusercontent.com/meadhikari/logstash/master/sample_indexer.conf -O /etc/logstash/conf.d/central.conf
 
+#downloading the init script for the server
+sudo wget https://raw.githubusercontent.com/meadhikari/logstash/master/logstash-shipper-init -O /etc/init.d/logstash-shipper-init
+#making the script executable
+sudo chmod +x ./etc/init.d/logstash-shipper-init
+#making it start at boot time
+sudo update-rc.d /etc/init.d/logstash-shipper-init defaults
+
+
 
 
 
