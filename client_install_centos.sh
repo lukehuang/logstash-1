0b3e24b30107e99ed87b7519102ff791a3f166da
Bikram Adhikari <salik.adhikari@gmail.com>
2014-03-26 20:20:33 +0545

    added installation script for centos

---
+++ client_install_centos.sh
@@ -0,0 +1,20 @@
+#adding of the logstash repository to the source list
+sudo rpm --import http://packages.elasticsearch.org/GPG-KEY-elasticsearch
+sudo wget https://raw.githubusercontent.com/meadhikari/logstash/master/logstash.repo -O /etc/yum.repos.d/logstash.repo
+
+#download the configuration from the github server different for each type of client but here only for the sample is done
+sudo wget https://raw.githubusercontent.com/meadhikari/logstash/master/shipper.conf -O /etc/logstash/conf.d/shipper.conf
+
+#geodb
+sudo mkdir /opt/geodb/
+sudo wget https://github.com/meadhikari/logstash/raw/master/GeoLiteCity.dat -O /opt/geodb/GeoLiteCity.dat
+
+#add pattern for jetty
+sudo wget https://raw.githubusercontent.com/meadhikari/logstash/master/jetty -O /opt/logstash/patterns/jetty
+#start in now
+sudo service logstash start
+
+#starting the logstash service at startup
+sudo /sbin/chkconfig --add mysqld
+sudo /sbin/chkconfig mysqld on
+
